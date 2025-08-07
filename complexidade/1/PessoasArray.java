65ad188897f512151ef8fb0ff39c196ab291d679
Maurilio Silva <mauriliodeveloper@gmail.com>
2025-08-07 21:11:32 +0000

    Teste

--- complexidade/1/PessoasArray.java
+++ complexidade/1/PessoasArray.java
@@ -3,7 +3,7 @@ public class PessoasArray {
     private String[] nomes;
 
     public PessoasArray() {
-        nomes =new String[]{"Amanda", "Beatriz", "Carlos","Daniela","Eduardo",
+        nomes = new String[]{"Amanda", "Beatriz", "Carlos","Daniela","Eduardo",
                 "Fabio","Gustavo", "Hingrid","Isabela","Joao","Leise","Maria",
                 "Norberto","Otavio","Paulo", "Quirino","Renata","Sabata",
                 "Tais","Umberto","Vanessa","Xavier"};
@@ -20,7 +20,6 @@ public class PessoasArray {
     // implementar o método de buscaLinear - O(n)
     public void buscaLinear(String nome){
         boolean encontrado = false;
-
         System.out.println("Procurando pelo nome: \"" + nome + "\"");
         for (int i = 0; i < nomes.length; i++) {
             System.out.println("Passando pelo indice:" + i);
